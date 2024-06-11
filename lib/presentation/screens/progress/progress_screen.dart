263401da90f74260031548023b4c4779070ea297
milton <miltondanielhch617@gmail.com>
2024-06-11 17:02:25 -0400

    progress indicators terminando

--- lib/presentation/screens/progress/progress_screen.dart
+++ lib/presentation/screens/progress/progress_screen.dart
@@ -32,7 +32,7 @@ class _ProgressView extends StatelessWidget {
           CircularProgressIndicator(
               strokeWidth: 2, backgroundColor: Colors.black45),
           SizedBox(height: 20),
-          Text('Circula indicator controlado'),
+          Text('Circular y Linear controlado'),
           SizedBox(height: 10),
           _ControlledProgressIndicator()
         ],
@@ -46,6 +46,28 @@ class _ControlledProgressIndicator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return StreamBuilder(
+        stream: Stream.periodic(const Duration(milliseconds: 300), (value) {
+          return (value * 2) / 100;
+        }).takeWhile((value) => value < 100),
+        builder: (context, snapshot) {
+          final progressValue = snapshot.data ?? 0;
+          return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircularProgressIndicator(
+                    value: progressValue,
+                    strokeWidth: 2,
+                    backgroundColor: Colors.black12,
+                  ),
+                  Expanded(
+                      child: LinearProgressIndicator(
+                    value: progressValue,
+                  ))
+                ],
+              ));
+        });
   }
 }
