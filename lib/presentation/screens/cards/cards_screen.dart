890dce02cb78fdeabb6f0d56dcac85ffe9893826
milton <miltondanielhch617@gmail.com>
2024-06-10 22:56:38 -0400

    agregando typo de tarjeta2 con borde

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 
 const cards = <Map<String, dynamic>>[
   {'elevation': 0.0, 'label': 'Elevation 0'},
-  {'elevation': 0.0, 'label': 'Elevation 0'},
-  {'elevation': 0.0, 'label': 'Elevation 0'},
-  {'elevation': 0.0, 'label': 'Elevation 0'},
-  {'elevation': 0.0, 'label': 'Elevation 0'},
-  {'elevation': 0.0, 'label': 'Elevation 0'},
+  {'elevation': 1.0, 'label': 'Elevation 1'},
+  {'elevation': 2.0, 'label': 'Elevation 2'},
+  {'elevation': 3.0, 'label': 'Elevation 3'},
+  {'elevation': 4.0, 'label': 'Elevation 4'},
+  {'elevation': 4.0, 'label': 'Elevation 5'},
 ];
 
 class CardsScreen extends StatelessWidget {
@@ -36,6 +36,10 @@ class _CardsView extends StatelessWidget {
             (card) =>
                 _CardType1(elevation: card['elevation'], label: card['label']),
           ),
+          ...cards.map(
+            (card) =>
+                _CardType2(elevation: card['elevation'], label: card['label']),
+          ),
         ],
       ),
     );
@@ -64,6 +68,41 @@ class _CardType1 extends StatelessWidget {
               ),
             ),
             Align(alignment: Alignment.bottomLeft, child: Text(label)),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CardType2 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CardType2({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return Card(
+      shape: RoundedRectangleBorder(
+          borderRadius: const BorderRadius.all(Radius.circular(12)),
+          side: BorderSide(color: colors.outline)),
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                icon: const Icon(Icons.more_vert_outlined),
+                onPressed: () {},
+              ),
+            ),
+            Align(
+                alignment: Alignment.bottomLeft,
+                child: Text('$label - outline')),
           ],
         ),
       ),
