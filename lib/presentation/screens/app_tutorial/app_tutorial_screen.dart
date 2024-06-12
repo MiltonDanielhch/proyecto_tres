10ff0e3d7e1d3ab569d593043e2f7c09fe43d407
milton <miltondanielhch617@gmail.com>
2024-06-12 14:43:19 -0400

    añadiendo app_tutorial_screen el pageview

--- lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -1,9 +1,62 @@
 import 'package:flutter/material.dart';
 
+class SlideInfo {
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  SlideInfo(
+      {required this.title, required this.caption, required this.imageUrl});
+}
+
+final slides = <SlideInfo>[
+  SlideInfo(
+      title: 'Busca la comida',
+      caption:
+          'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500,',
+      imageUrl: 'assets/images/1.png'),
+  SlideInfo(
+      title: 'Entrega Rapido',
+      caption:
+          'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500,',
+      imageUrl: 'assets/images/2.png'),
+  SlideInfo(
+      title: 'Disfruta la comida',
+      caption:
+          'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500,',
+      imageUrl: 'assets/images/3.png'),
+];
+
 class AppTutorialScreen extends StatelessWidget {
   static const name = 'tutorial_screen';
 
   const AppTutorialScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        physics: const BouncingScrollPhysics(),
+        children: slides
+            .map(
+              (slideData) => _Slide(
+                  title: slideData.title,
+                  caption: slideData.caption,
+                  imageUrl: slideData.imageUrl),
+            )
+            .toList(),
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  const _Slide(
+      {required this.title, required this.caption, required this.imageUrl});
 
   @override
   Widget build(BuildContext context) {
