92cef4a24c455e6364c0457c23f897b8f3fef926
milton <miltondanielhch617@gmail.com>
2024-06-13 12:45:21 -0400

    cambiar valores y modificaciones de claro y oscuro

---
+++ lib/presentation/providers/theme_provider.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+//estado => isDarkmodeProvider = boolean
+
+final isDarkmodeProvider = StateProvider((ref) => false);
--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:proyecto_tres/presentation/providers/counter_provider.dart';
+import 'package:proyecto_tres/presentation/providers/theme_provider.dart';
 
 class CounterScreen extends ConsumerWidget {
   static const name = 'counter_screen';
@@ -9,9 +10,21 @@ class CounterScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final int clickCounter = ref.watch(counterProvider);
+    final bool isDarkmode = ref.watch(isDarkmodeProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Counter Screen'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              ref.read(isDarkmodeProvider.notifier).update((state) => !state);
+            },
+            // icon: Icon(Icons.light_mode_outlined),
+            icon: Icon(isDarkmode
+                ? Icons.dark_mode_outlined
+                : Icons.light_mode_outlined),
+          ),
+        ],
       ),
       body: Center(
         child: Text(
@@ -21,7 +34,10 @@ class CounterScreen extends ConsumerWidget {
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () {
+          // ref.read(counterProvider.notifier).update((state) => state + 1);
+          ref.read(counterProvider.notifier).state++;
+        },
       ),
     );
   }
